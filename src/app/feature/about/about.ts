4cab633aaf8ae9808550e0f70ff30b66f65f2dc4
abdelawal <abdelawal89@gmail.com>
2025-12-04 07:26:36 +0200

    feat: Scaffold Angular application with main pages, shared components, and initial configurations.

--- src/app/feature/about/about.ts
+++ src/app/feature/about/about.ts
@@ -2,21 +2,18 @@ import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 
 
-import { RouterLink } from "@angular/router";
-
-
 @Component({
   selector: 'app-about',
-  imports: [ RouterLink],
+  imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
-  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: any) { }
 
-callNow() {
-  if (isPlatformBrowser(this.platformId)) {
-    window.open("tel:3179439599");
+  callNow() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.open("tel:3179439599");
+    }
   }
-}
 }
